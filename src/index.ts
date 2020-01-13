bfc3a9b76e09b84fcd745716bdf8c639671f1bf5
lduburas <laduem@gmail.com>
2020-01-13 11:07:25 +0200

    Empty values fix

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import transformer from './transformer';
+import { isUndefined } from 'util';
 
 export declare function toDates<T extends object>(z: any): void;
 
-export function toDatesByArray(value: any, props: string[][]) {
-    props.forEach(path => {
-        path.reduce((obj: any, prop: string, i: number) => {
-            if (i + 1 < path.length)
-                return obj[prop];
-            else
-                obj[prop] = new Date(obj[prop]);
-        }, value);
+export function toDatesByArray(value: any, paths: string[][]) {
+    paths.forEach(path => {
+        const last = path.length - 1;
+        let i = 0;
+        let obj = value;
+
+        while (obj && i < last) {
+            obj = obj[path[i++]];
+        }
+
+        if (obj && i === last) {
+            const dateValue = obj[path[last]];
+            if (!isUndefined(dateValue))
+                obj[path[last]] = dateValue ? new Date(dateValue) : null;
+        }
     });
     return value;
 }
